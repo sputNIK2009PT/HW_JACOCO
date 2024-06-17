66a72dc2bbb8e6b832e95a13a46feeb0eeb454e3
Alexander Stupnikov <iam-not@mail.ru>
2024-06-17 17:07:39 +0300

    Jacoco incomplete covering

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -12,18 +12,17 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMax2() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 13, 12};
         long expected = 13;
-
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
